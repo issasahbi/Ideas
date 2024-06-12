bee6ac3ee0318dc021547a565a289c3f8cfe25ae
issasahbi <sahbi-issa@hotmail.com>
2024-06-12 15:30:08 +0200

    add Idea model and data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view("dashboard");
+        return view("dashboard", ['ideas' => Idea::all()]);
     }
 }
